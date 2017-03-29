f699a7a46556dd175176ecd51da92c7a1a40a65a
wchang28 <wchang28@hotmail.com>
2017-03-28 22:38:58 -0400

    first comment

--- ts/app.ts
+++ ts/app.ts
@@ -41,7 +41,7 @@ let targetAcquisition: httpProxy.TargetAcquisition = (req:express.Request) => {
 let eventEmitter = new events.EventEmitter();
 
 eventEmitter.on('error', (err: any) => {
-    console.error(new Date().toISOString() + ": !!! Proxy error: " + err.toString());
+    console.error(new Date().toISOString() + ": !!! Proxy error: " + JSON.stringify(err));
 });
 
 app.use(httpProxy.get({targetAcquisition, eventEmitter}));
